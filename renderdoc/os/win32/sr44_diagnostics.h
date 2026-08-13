527306f0a44c9c597c8552e9fe10d10023471165
hujiahao <00.0.mr.master.0.00@gmail.com>
2026-08-13 16:00:00 +0800

    feat(injection): inject suspended processes with SetThreadContext

---
+++ renderdoc/os/win32/sr44_diagnostics.h
@@ -0,0 +1,7 @@
+/******************************************************************************
+ * SR 4.4 diagnostics are opt-in and only record lifecycle information.
+ ******************************************************************************/
+#pragma once
+
+void LogSR44LaunchStage(const char *stage, const char *details);
+
